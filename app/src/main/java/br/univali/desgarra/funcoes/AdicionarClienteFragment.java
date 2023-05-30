d45ba7d21451626e313903f8d0f113c7390113d0
7634862 <yuri_rodrigues@edu.univali.br>
2023-05-30 16:46:12 -0300

    v3

---
+++ app/src/main/java/br/univali/desgarra/funcoes/AdicionarClienteFragment.java
@@ -0,0 +1,30 @@
+package br.univali.desgarra.funcoes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import br.univali.desgarra.R;
+
+public class AdicionarClienteFragment extends Fragment {
+
+
+    public AdicionarClienteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_adicionar_cliente, container, false);
+    }
+}
